724cec8d6735e9a6f5688130c0b386e9d80adcaf
HamishWatters <hamishw13@gmail.com>
2016-03-23 01:19:56 +0000

    Improves query entry advanced / simple forms working together

--- static/js/querybuilder.js
+++ static/js/querybuilder.js
@@ -2,21 +2,33 @@ $(document).ready( function() {
 	var no = 0;
 	var curQue = ""
 	$('button[id="addButton"]').click( function() {
-		no = no + 1;
+	curQue = $('textarea').val()
+		no = no + 1
+		$('textarea[id="queryIDb"]').val($('textarea[id="queryID"]').val());
 		var toAdd = $('input[name=queryBox]').val();
 		var type = $('select[name=fieldType]').val();
 		var eType = $('select[name=entryType]').val();
 		if (no == 1) {
-			$('.queries').html('<div class="item">'  + toAdd + type + " " + '</div>');
-			curQue = (toAdd + type);
+			if ($('textarea').val() == ""){
+				curQue = ($('textarea').val() + toAdd + type);
+				}
+			else 
+				{
+				curQue = ("(" + $('textarea').val() + ") " + eType + " " + toAdd + type)
+				}
 		} else{
-				$('.queries').html('<div class="item">(' + curQue + ") "  + eType + " " + toAdd +type + ' </div>');
 				curQue = ('(' + curQue + ") "  + eType + " " + toAdd + type);
 		}
-		$('textarea[id="queryID"]').val('(' + curQue + ")") 
+		$('textarea').val('(' + curQue + ")") 
 	});
 	$('button[id="queryAddButton"]').click( function() {
-		$('textarea[id="queryID"]').prepend($('select[name=entryType]').val() + " (");
 		$('textarea[id="queryID"]').append($(this).val() + ")");
+		$('textarea[id="queryID"]').prepend($('select[name=entryType]').val() + " (");
+	});
+	
+	$('form[id="saveButton"]').click(function(){
+		$('textarea[id="queryIDb"]').val($('textarea[id="queryID"]').val());
 	});
 });
+
+
